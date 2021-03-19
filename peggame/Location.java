f8cd9c5e39ca75fc0586acfa23e713db2229be3f
karisanno11 <ks8884@g.rit.edu>
2021-03-19 17:56:19 -0400

    loco

--- peggame/Location.java
+++ peggame/Location.java
@@ -1,5 +1,20 @@
-package project;
+package peggame;
 
 public class Location{
+    private final int row;
+    private final int col;
+
+    public Location(int row, int col){
+        this.row = row;
+        this.col = col;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
+    public int getRow() {
+        return row;
+    }
     
 }
