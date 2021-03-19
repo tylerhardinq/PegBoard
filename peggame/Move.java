024f154beb06d602860a023be3407127676eb3ed
tylerharding.work@gmail.com <tylerharding.work@gmail.com>
2021-03-19 18:10:38 -0400

    Added Move and updated location

---
+++ peggame/Move.java
@@ -0,0 +1,30 @@
+package peggame;
+
+public class Move {
+    private Location from;
+    private Location to;
+    
+    public Move(Location from, Location to) {
+        this.from = from;
+        this.to = to;
+    }
+    
+    public Location getFrom() {
+        return from;
+    }
+    
+    public Location getTo() {
+        return to;
+    }
+    
+    public boolean equals(Object o) {
+        if (!(o instanceof Move)) {
+        return false;
+        }
+    
+        Move other = (Move)o;
+    
+        return(from == other.from && to == other.to);
+    
+    }
+}
