faa1af028b14a072f9cd9d6f2dd75eb90bd3acfc
a1484414286 <ll6209@rit.edu>
2021-03-19 18:02:25 -0400

    saving progress

---
+++ peggame/PegGame.java
@@ -0,0 +1,9 @@
+import java.util.Collection;
+
+public interface PegGame {
+    Collection<Move> getPossibleMoves();
+    GameState getGameState();
+    default void makeMove(Move move) {
+        throw new PegGameException();
+    }
+}
