72fb956700ccc22e3735080a15b7007b135ac4eb
a1484414286 <ll6209@rit.edu>
2021-03-19 18:13:37 -0400

    ya

--- peggame/PegGame.java
+++ peggame/PegGame.java
@@ -1,8 +1,13 @@
+package peggame;
+
+
 import java.util.Collection;
 
 public interface PegGame {
     Collection<Move> getPossibleMoves();
     GameState getGameState();
+
+
     default void makeMove(Move move) {
         throw new PegGameException();
     }
---
+++ peggame/PegGameException.java
@@ -0,0 +1,8 @@
+package peggame;
+import java.lang.Exception;
+
+public class PegGameException {
+    public PegGameException(String message){
+        Exception(String message);
+    }
+}
