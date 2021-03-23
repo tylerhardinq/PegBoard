3480919b4b4d4f45029199547456cb377d79751f
tylerharding.work@gmail.com <tylerharding.work@gmail.com>
2021-03-22 23:32:10 -0400

    e

--- peggame/ghjkl.java
+++
@@ -1,9 +0,0 @@
-package peggame;
-
-public class ghjkl {
-    public static void main(String[] args) {
-        String yes = "uishefuiuiadabwd";
-        System.out.println(yes.split("")[2]);
-    }
-    
-}
