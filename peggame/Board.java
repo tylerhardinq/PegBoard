eb8068af72ed01736abe4d9ebfd74d0251994c87
karisanno11 <ks8884@g.rit.edu>
2021-03-23 00:11:47 -0400

    readfile made

--- peggame/Board.java
+++ peggame/Board.java
@@ -1,5 +1,9 @@
 package peggame;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.Collection;
 
@@ -16,7 +20,7 @@ public class Board implements PegGame{
         this.rows = rows;
         this.cols = cols;
         this.board = new String[rows][cols];
-        board[0][0] = "-";
+        // board[0][0] = "-";
         moves = 0;
         this.totalPeg = (rows * cols)-1;
         this.state = GameState.NOT_STARTED;
@@ -35,7 +39,7 @@ public class Board implements PegGame{
     }
 
     public boolean moveValid(Move move){
-
+        return false;
     }
     @Override
     public void makeMove(Move move) {
@@ -73,13 +77,13 @@ public class Board implements PegGame{
     
     @Override 
     public GameState getGameState(){
-        return null;
+        return state;
 
     }
 
-
-
-
+    public int getTotalPeg() {
+        return totalPeg;
+    }
 
     public int getMoves() {
         return moves;
@@ -92,11 +96,11 @@ public class Board implements PegGame{
         for(int row=0; row<rows; row++) {
             for(int col=0; col<cols; col++) {
                 builder.append("[");
-                if(board[row][col] == null){
-                    builder.append("o");
+                if(board[row][col] == "."){
+                    builder.append("-"); 
                 }
                 else{
-                    builder.append("-"); 
+                    builder.append(board[row][col]); 
                 }
                
                 builder.append("]");
@@ -106,11 +110,48 @@ public class Board implements PegGame{
         return builder.toString();
     }
 
+    public static Board readFromFile(String filename) throws FileNotFoundException {
+        FileReader fr = new FileReader(filename);
+        BufferedReader br = new BufferedReader(fr);
+        try {
+            String[] rowCol = br.readLine().split(" ");
+            if(rowCol.length != 1) {
+                int row = Integer.parseInt(rowCol[0]);
+                int col = Integer.parseInt(rowCol[1]);
+                Board filledBoard = new Board(row , col);
+                // fill the board 
+                String line = "";
+                while(line != null) {
+                    for(int r = 0; r < row; r++) {
+                        line = br.readLine();
+                        String[] a =line.split("");
+                        for(int c = 0; c < col; c++) {
+                            filledBoard.board[r][c] = a[c] ;
+                        }
+                    }
+                    return filledBoard;
+                    // Still need to make an else for if the row and col are different.
+                }
+                br.close();
 
-    public static void main(String[] args) {
-        Board board = new Board(8, 8);
+            }
+
+            
+
+        } catch (IOException ioe) {
+            System.out.println(ioe);
+        }
+        return null;
+
+    }
+
+
+    public static void main(String[] args) throws FileNotFoundException {
+        Board board = new Board(4, 4);
         // board.makeMove(5, 7);
-        System.out.println(board);
+        // System.out.println(board);
+        System.out.println(readFromFile("data/3_3.txt"));
+
     }
     
 }
