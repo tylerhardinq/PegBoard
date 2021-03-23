d48668a7492cd213b82c1acc684f6396fec42d82
karisanno11 <ks8884@g.rit.edu>
2021-03-22 22:35:37 -0400

    sorry

--- peggame/Board.java
+++ peggame/Board.java
@@ -1,21 +1,25 @@
 package peggame;
 
 import java.util.Arrays;
+import java.util.Collection;
 
-public class Board {
+public class Board implements PegGame{
 
     private final int rows;
     private final int cols;
-
     private final String[][] board;
-
-    private int moves;
+    private int moves;   //total moves
+    private int totalPeg; //total number of pigs on the board
+    private GameState state; //The current state of the game
 
     public Board(int rows, int cols) {
         this.rows = rows;
         this.cols = cols;
         this.board = new String[rows][cols];
+        board[0][0] = "-";
         moves = 0;
+        this.totalPeg = (rows * cols)-1;
+        this.state = GameState.NOT_STARTED;
     }
 
     public Board(Board template) {
@@ -30,25 +34,57 @@ public class Board {
         this.moves = template.moves;
     }
 
-    public boolean makeMove(int row, int col) {
-        if(row < 0 || row >= rows
-            || col < 0 || col >= cols
-            || board[row][col] != "-") {
-            return false;
-        } else {
-            moves++;
-            board[row][col] = "o";
-            return true;
-        }
+    public boolean moveValid(Move move){
+
     }
+    @Override
+    public void makeMove(Move move) {
+        //what makes it valid?
+
+        /**
+         * 
+         * 
+         * 
+         * 
+         * 
+         * 
+         */
+
+
+
+
+
+
+        // if(move.getTo()row < 0 || row >= rows
+        //     || col < 0 || col >= cols
+        //     || board[row][col] != "-") {
+        //     return false;
+        // } else {
+        //     moves++;
+        //     board[row][col] = "o";
+        //     totalPeg--;
+        //     return true;
+        // }
+    }
+    @Override 
+    public Collection<Move> getPossibleMoves(){
+        return null;
+    }
+    
+    @Override 
+    public GameState getGameState(){
+        return null;
+
+    }
+
+
+
+
 
     public int getMoves() {
         return moves;
     }
 
-    public boolean isFull() {
-        return moves == (rows * cols);
-    }
 
     @Override
     public String toString() {
@@ -58,6 +94,9 @@ public class Board {
                 builder.append("[");
                 if(board[row][col] == null){
                     builder.append("o");
+                }
+                else{
+                    builder.append("-"); 
                 }
                
                 builder.append("]");
@@ -70,7 +109,7 @@ public class Board {
 
     public static void main(String[] args) {
         Board board = new Board(8, 8);
-        board.makeMove(5, 7);
+        // board.makeMove(5, 7);
         System.out.println(board);
     }
     
---
+++ peggame/BoardTest.java
@@ -0,0 +1,5 @@
+package peggame;
+
+public class BoardTest {
+    
+}
