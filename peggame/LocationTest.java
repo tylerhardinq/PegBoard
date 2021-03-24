e93816a6719d6836e004615fa2b67e7513540fac
tylerharding.work@gmail.com <tylerharding.work@gmail.com>
2021-03-24 19:00:14 -0400

    added location test

---
+++ peggame/LocationTest.java
@@ -0,0 +1,25 @@
+package peggame;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.Test;
+import org.junit.platform.commons.annotation.Testable;
+
+@Testable
+public class LocationTest {
+    
+    @Test
+    public void testLocation() {
+        Location l1 = new Location(0, 0);
+        Location l2 = new Location(2, 2);
+        Location l3 = new Location(2, 2);
+
+        assertEquals(0, l1.getRow());
+        assertEquals(0, l1.getCol());
+        assertEquals(2, l2.getRow());
+        assertEquals(2, l2.getCol());
+        assertEquals(l2, l3);
+
+
+    }
+}
