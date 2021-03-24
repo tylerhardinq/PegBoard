891a91dd3a91d7cb4c4eabfdeb6898d14bad2f34
tylerharding.work@gmail.com <tylerharding.work@gmail.com>
2021-03-24 17:55:23 -0400

    added CLI

---
+++ peggame/CommandLines.java
@@ -0,0 +1,44 @@
+package peggame;
+import java.util.Scanner;
+
+public class CommandLines {
+    public static void CLI(Game game) {
+        Scanner sc = new Scanner(System.in);
+        boolean flag = true;
+
+        while(flag) {
+        System.out.println(game.getBoard());
+        System.out.print(">> ");
+        String input = sc.nextLine();
+
+        if(input.equals("help")) {
+            System.out.println("help - displays this message");
+            System.out.println("move r1 c1 r2 c2 - attempts to move a peg from r1 c1 to r2 c2 on the board.");
+            System.out.println("hint - displays an available move.");
+            System.out.println("quit - quits the game");
+        }
+
+        if(input.equals("hint")) {
+            System.out.println(game.getPossibleMoves());
+        }
+
+        if(input.equals("quit")) {
+            System.out.println("Are you sure? (y/n)");
+            input = sc.nextLine();
+            if (input.equals("y")) {
+                flag = false;
+                System.out.println("Goodbye.");
+            }
+        }
+
+        String[] tokens = input.split(" ");
+        if(tokens[0].equals("move") && tokens.length == 5) {
+            Location from = new Location(Integer.parseInt(tokens[1]), Integer.parseInt(tokens[2]));
+            Location to = new Location(Integer.parseInt(tokens[3]), Integer.parseInt(tokens[4]));
+            game.makeMove(new Move(from, to));
+        }
+
+
+
+    }
+}
