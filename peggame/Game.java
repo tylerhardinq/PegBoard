4df94fbd9e671498e3ceef94ab0c99cd920f1c04
karisanno11 <ks8884@g.rit.edu>
2021-03-25 15:52:18 -0400

    Commented

--- peggame/Game.java
+++ peggame/Game.java
@@ -5,19 +5,34 @@ import java.util.Collection;
 import java.util.LinkedList;
 import java.util.List;
 
+/**
+ * This class contains methods to play the PegGame and 
+ * checking the state of the board.
+ * We seperated what we were told to do in CLI(Part6) into this class
+ */
 public class Game implements PegGame{
     private Board board;
 
+    /**
+     * @param boar Board that is going to be played on.
+     */
     public Game(Board boar){
         this.board = boar;
     }
     
+    /**
+     * moves a peg on the board from and to a specified location.
+     * peg is removed from the from location and moved to the to location.
+     * 
+     * @param move Contains Location from and to
+     */
     @Override
     public void makeMove(Move move) throws PegGameException {
-        if(getPossibleMoves().contains(move)){
-            board.board[move.getTo().getRow()][move.getTo().getCol()] = "o";
-            board.board[move.getFrom().getRow()][move.getFrom().getCol()] = ".";
+        if(getPossibleMoves().contains(move)){  //If the move is a valid move
+            board.board[move.getTo().getRow()][move.getTo().getCol()] = "o";     //The To gets a peg
+            board.board[move.getFrom().getRow()][move.getFrom().getCol()] = ".";  //The peg in the from is removed
 
+            //Finding location in the middle
             int x1 = move.getFrom().getRow();
             int x2 = move.getTo().getRow();
             int y1 = move.getFrom().getCol();
@@ -25,9 +40,10 @@ public class Game implements PegGame{
             int x = (x1+x2)/2;
             int y = (y1+y2)/2;
             board.board[x][y] = ".";
-            board.addMove();
-            board.removePeg();
+            board.addMove();  //Increments the number of move made
+            board.removePeg();  //removes one peg from the totalPegs on the board
 
+            //Changes the state of the board based on the total number of pegs and moves
             if(getPossibleMoves().size() == 0 && board.getTotalPeg() == 1) {
                 board.setState(GameState.WON);
             } else if(board.getTotalPeg() >= 2 && getPossibleMoves().size() == 0) {
@@ -38,24 +54,36 @@ public class Game implements PegGame{
             
         }
         else{
-            throw new PegGameException("invalid move haha ");
+            //Invalid move is made
+            throw new PegGameException("invalid move :(");
         }
     }
-    
+    /**
+     * @return returns the current board
+     */
     public Board getBoard() {
         return board;
     }
 
+    /**
+     * Checks where the empty hole is on the board and makes
+     * a collection of possible moves that could be made horizontally,
+     * vertically and diagnally.
+     */
     @Override
     public Collection<Move> getPossibleMoves() {
         
-        Collection<Move> possibleMoves = new LinkedList<>();
-        List<Location> To = new LinkedList<>();
+        Collection<Move> possibleMoves = new LinkedList<>();  //new empty collection
+        List<Location> To = new LinkedList<>();  //list of empty holes which will be the to location
+        //inner is the possible location between the from and to
+        //outer is the possible from location
         int[] innerRow = {0, 1, 1,  1,  0, -1, -1, -1};
         int[] innerCol = {1, 1, 0, -1, -1, -1,  0,  1};
         int[] outerRow = {0, 2, 2, 2,   0, -2, -2, -2};
         int[] outerCol = {2, 2, 0, -2, -2, -2,  0,  2};
         
+
+        //adding the location of empty holes
         if(board.getTotalPeg() != 1){
             for(int r = 0; r < board.getRows(); r++) {
                 for(int c = 0; c < board.getCols(); c++) {
@@ -65,9 +93,12 @@ public class Game implements PegGame{
                 }
             }
         }
+
+        //checking each empty location 
         for(Location i : To){
             for(int j = 0; j < innerCol.length; j++) {
                 
+                //possible location of the from and location betweeen from and to 
                 int nextOuterR = i.getRow() + outerRow[j];
                 int nextOuterC = i.getCol() + outerCol[j];
                 int nextInnerR = i.getRow() + innerRow[j];
@@ -76,8 +107,8 @@ public class Game implements PegGame{
                 Location nextOuterLocation = new Location(nextOuterR, nextOuterC);
                 Location nextInnerLocation = new Location(nextInnerR, nextInnerC);
                 
-                
-                if(isValid(nextOuterLocation) && hasPeg(nextOuterLocation)) {
+                //Checks if the locations are valid and adds it to the possible moves
+                if(isValid(nextOuterLocation) && hasPeg(nextOuterLocation)) {  
                     if(isValid(nextInnerLocation) && hasPeg(nextInnerLocation)) {
                         Move move = new Move(nextOuterLocation, i);
                         possibleMoves.add(move);
@@ -90,7 +121,12 @@ public class Game implements PegGame{
     }
 
 
-
+    /**
+     * checks if location contains a peg and if not returns false
+     * 
+     * @param location location that is going to be checked
+     * @return
+     */
     public boolean hasPeg(Location location) {
         if(board.board[location.getRow()][location.getCol()].equals("o")) {
             return true;
@@ -98,6 +134,12 @@ public class Game implements PegGame{
         return false;
     }
 
+    /**
+     * checks if a location is inside the board
+     * 
+     * @param loc location that is going ot be checked
+     * @return
+     */
     public boolean isValid(Location loc) {
         if(loc.getRow() < 0 || loc.getRow() >= board.getRows()) {
             return false;
@@ -108,11 +150,19 @@ public class Game implements PegGame{
         return true;
     }
 
+    /**
+     * returns the current gamestate of the board
+     */
     @Override
     public GameState getGameState() {
         return board.getState();
     }
     
+    /**
+     * Manual test of this class
+     * @throws FileNotFoundException
+     * @throws PegGameException
+     */
     public static void main(String[] args) throws FileNotFoundException, PegGameException {
         new BoardFromFile();
         Game game = new Game(BoardFromFile.readFromFile("data/4_4.txt"));
